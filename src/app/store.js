4fe56caa20031f5141b150fb83ebbbf56b7e5d4d
lakshay9763 <lakshay9763@gmail.com>
2026-05-18 15:46:48 +0530

    Update store.js

--- src/app/store.js
+++ src/app/store.js
@@ -5,7 +5,7 @@ import { residentSlice } from "../features/resident/residentSlice";
 import { guardSlice } from "../features/guard/guardSlice";
 import { deviceSlice } from "../features/devices/deviceSlice";
 import { staffSlice } from "../features/staff/staff.slice";
-import { logSlice } from "../features/logs/logs.slice";
+import { logSlice } from "../features/auth/logs.slice";
 import { visitorSlice } from "../features/visitor/vis.slice";
 import { utilitySlice } from "../features/utility_worker/utility.slice";
 import { authSlice } from "../features/auth/auth.slice";
